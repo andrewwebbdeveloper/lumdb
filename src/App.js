c759f862bc26e27e44fbd655e0782f5be687f83e
Andrew Webb <andrew@Andrews-MacBook-Pro.local>
2019-03-05 17:04:03 -0500

    added linter in 13

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import './App.css';
 
 import Movie from './Movie'
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-    desc: 'A galaxy far far away'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  }, {
-    id: 4,
-    title: '5 Deadly Venoms'
-  },
-];
-
 class App extends Component {
+  state = {
+    movies: [],
+
+  }
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=db2cc553340037f8975582668d878841&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      })
+
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
