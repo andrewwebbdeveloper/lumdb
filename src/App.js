5e1d451246762dd3bea2a58145f073e915fb2874
Andrew Webb <andrew@Andrews-MacBook-Pro.local>
2019-03-06 15:37:10 -0500

    added linter in 13 and 14

--- src/App.js
+++ src/App.js
@@ -2,41 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie'
+import Movie from './Movie';
 
 class App extends Component {
   state = {
     movies: [],
 
   }
+
   async componentDidMount() {
     try {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=db2cc553340037f8975582668d878841&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
-      console.log(movies);
+      // console.log(movies);
       this.setState({
-        movies: movies.results
-      })
-
-
+        movies: movies.results,
+      });
     } catch (e) {
-      console.log(e)
+      // console.log(e);
     }
   }
 
   render() {
-
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
 }
-
 
 
 export default App;
