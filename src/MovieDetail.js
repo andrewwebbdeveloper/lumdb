fb81ca99b7cb7805fb73dd651704e5fadc7b2771
Andrew Webb <andrew@Andrews-MacBook-Pro.local>
2019-03-07 09:34:53 -0500

    Through 19

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+import { Poster } from './Movie';
 
 const POSTER_PATH = ' http://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = ' http://image.tmdb.org/t/p/w1280';
@@ -27,13 +29,18 @@ class MovieDetail extends Component {
     const { movie } = this.state;
     return (
 
-      <div>
-        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-        <h1>{movie.title}</h1>
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </div>
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          </Overdrive>
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
 
 
     );
@@ -42,3 +49,25 @@ class MovieDetail extends Component {
 
 
 export default MovieDetail;
+
+
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background: url(${props => props.backdrop}) no-repeat; 
+  background-color: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white; 
+  text-align: left; 
+  padding: 2rem 10%; 
+  display: flex; 
+  > div {
+    margin-left: 20px; 
+  }
+  img {
+    position: relative;
+    top: -5rem; 
+  }
+`;
